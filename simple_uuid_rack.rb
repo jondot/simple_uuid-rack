733b044da5f25cc26c8ca3316599c6a46353dff0
Dotan Nahum <jondotan@gmail.com>
2012-01-10 19:51:12 +0200

    allow all

--- simple_uuid_rack.rb
+++ simple_uuid_rack.rb
@@ -3,7 +3,7 @@ require 'simple_uuid'
 module Rack
   class SimpleUUID
     def call(env)
-      [404, {'Content-Type' => 'text/json'}, [::SimpleUUID::UUID.new.to_guid]]
+      [200, {'Content-Type' => 'text/json', 'Access-Control-Allow-Origin'=>'*'}, [::SimpleUUID::UUID.new.to_guid]]
     end
   end
 end
